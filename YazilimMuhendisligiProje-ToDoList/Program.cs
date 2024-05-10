08027a8acbd25fa236ca07b8b9314f4b30e44ba5
Mert Ömer BAKAR <95905493+mertomer@users.noreply.github.com>
2024-05-10 17:45:58 +0300

    Ufak değişiklikler

--- YazilimMuhendisligiProje-ToDoList/Program.cs
+++ YazilimMuhendisligiProje-ToDoList/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using YazilimMuhendisligiProje_ToDoList.Presentation;
 
 namespace YazilimMuhendisligiProje_ToDoList
 {
@@ -16,7 +17,7 @@ namespace YazilimMuhendisligiProje_ToDoList
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmRegister());
+            Application.Run(new frmLogin());
         }
     }
 }
