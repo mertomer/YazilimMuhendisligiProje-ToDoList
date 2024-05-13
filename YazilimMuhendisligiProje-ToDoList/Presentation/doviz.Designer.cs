db2bfb9104d340ed3d6eec8216d54289a28cbbbc
ozangkr <62ozngkr@gmail.com>
2024-05-13 20:59:47 +0300

    doviz kuru api başlangıçı

---
+++ YazilimMuhendisligiProje-ToDoList/Presentation/doviz.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace YazilimMuhendisligiProje_ToDoList.Presentation
+{
+    partial class doviz
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dolarAlisText = new System.Windows.Forms.Label();
+            this.dolarSatisText = new System.Windows.Forms.Label();
+            this.euroAlisText = new System.Windows.Forms.Label();
+            this.euroSatisText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Aqua;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(53, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dolar Alış :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Aqua;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(53, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 30);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Dolar Satış :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Aqua;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(53, 255);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 30);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Dolar Satış :";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Aqua;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(53, 190);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(141, 30);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Euro Alış :";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // dolarAlisText
+            // 
+            this.dolarAlisText.BackColor = System.Drawing.Color.Aqua;
+            this.dolarAlisText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dolarAlisText.Location = new System.Drawing.Point(242, 61);
+            this.dolarAlisText.Name = "dolarAlisText";
+            this.dolarAlisText.Size = new System.Drawing.Size(141, 30);
+            this.dolarAlisText.TabIndex = 4;
+            this.dolarAlisText.Click += new System.EventHandler(this.dolarAlis_Click);
+            // 
+            // dolarSatisText
+            // 
+            this.dolarSatisText.BackColor = System.Drawing.Color.Aqua;
+            this.dolarSatisText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dolarSatisText.Location = new System.Drawing.Point(242, 126);
+            this.dolarSatisText.Name = "dolarSatisText";
+            this.dolarSatisText.Size = new System.Drawing.Size(141, 30);
+            this.dolarSatisText.TabIndex = 5;
+            this.dolarSatisText.Click += new System.EventHandler(this.dolarSatisText_Click);
+            // 
+            // euroAlisText
+            // 
+            this.euroAlisText.BackColor = System.Drawing.Color.Aqua;
+            this.euroAlisText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.euroAlisText.Location = new System.Drawing.Point(242, 190);
+            this.euroAlisText.Name = "euroAlisText";
+            this.euroAlisText.Size = new System.Drawing.Size(141, 30);
+            this.euroAlisText.TabIndex = 6;
+            this.euroAlisText.Click += new System.EventHandler(this.euroAlisText_Click);
+            // 
+            // euroSatisText
+            // 
+            this.euroSatisText.BackColor = System.Drawing.Color.Aqua;
+            this.euroSatisText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.euroSatisText.Location = new System.Drawing.Point(242, 255);
+            this.euroSatisText.Name = "euroSatisText";
+            this.euroSatisText.Size = new System.Drawing.Size(141, 30);
+            this.euroSatisText.TabIndex = 7;
+            this.euroSatisText.Click += new System.EventHandler(this.euroSatisText_Click);
+            // 
+            // doviz
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.BackgroundImage = global::YazilimMuhendisligiProje_ToDoList.Properties.Resources.e_doviz_2_1024x638;
+            this.ClientSize = new System.Drawing.Size(613, 372);
+            this.Controls.Add(this.euroSatisText);
+            this.Controls.Add(this.euroAlisText);
+            this.Controls.Add(this.dolarSatisText);
+            this.Controls.Add(this.dolarAlisText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "doviz";
+            this.Text = "Döviz Kuru İşlemleri";
+            this.Load += new System.EventHandler(this.doviz_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label dolarAlisText;
+        private System.Windows.Forms.Label dolarSatisText;
+        private System.Windows.Forms.Label euroAlisText;
+        private System.Windows.Forms.Label euroSatisText;
+    }
+}
---
+++ YazilimMuhendisligiProje-ToDoList/Presentation/doviz.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace YazilimMuhendisligiProje_ToDoList.Presentation
+{
+    public partial class doviz : Form
+    {
+        public doviz()
+        {
+            InitializeComponent();
+        }
+
+        private void doviz_Load(object sender, EventArgs e)
+        {
+            String bugun = "https://www.tcmb.gov.tr/kurlar/today.xml";
+            var xmldosya = new XmlDocument();
+            xmldosya.Load(bugun);
+            
+            string dolarAlis = xmldosya.SelectSingleNode("Tarih_Date/Currency[@Kod='USD']/BanknoteBuying").InnerXml;
+            dolarAlisText.Text = dolarAlis;
+
+            string dolarsatis = xmldosya.SelectSingleNode("Tarih_Date/Currency[@Kod='USD']/BanknoteSelling").InnerXml;
+            dolarSatisText.Text = dolarsatis;
+
+            string euroalis = xmldosya.SelectSingleNode("Tarih_Date/Currency[@Kod='EUR']/BanknoteBuying").InnerXml;
+            euroAlisText.Text = euroalis;
+
+            string eurosatis = xmldosya.SelectSingleNode("Tarih_Date/Currency[@Kod='EUR']/BanknoteSelling").InnerXml;
+            euroSatisText.Text = eurosatis;
+
+        }
+
+        private void dolarAlis_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dolarSatisText_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void euroAlisText_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void euroSatisText_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
