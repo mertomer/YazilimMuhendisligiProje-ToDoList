fc809121b922850cb86c294802ac48545daff931
ozangkr <62ozngkr@gmail.com>
2024-06-03 21:34:56 +0300

    v2

--- YazilimMuhendisligiProje-ToDoList/Presentation/frmcoin.Designer.cs
+++ YazilimMuhendisligiProje-ToDoList/Presentation/frmcoin.Designer.cs
@@ -41,19 +41,19 @@ namespace YazilimMuhendisligiProje_ToDoList.Presentation
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(16, 15);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Location = new System.Drawing.Point(18, 15);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(1035, 463);
+            this.dataGridView1.Size = new System.Drawing.Size(1164, 463);
             this.dataGridView1.TabIndex = 0;
             // 
             // btnFetchData
             // 
-            this.btnFetchData.Location = new System.Drawing.Point(951, 511);
-            this.btnFetchData.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnFetchData.Location = new System.Drawing.Point(1070, 511);
+            this.btnFetchData.Margin = new System.Windows.Forms.Padding(4);
             this.btnFetchData.Name = "btnFetchData";
-            this.btnFetchData.Size = new System.Drawing.Size(100, 28);
+            this.btnFetchData.Size = new System.Drawing.Size(112, 28);
             this.btnFetchData.TabIndex = 1;
             this.btnFetchData.Text = "Fetch Data";
             this.btnFetchData.UseVisualStyleBackColor = true;
@@ -61,12 +61,14 @@ namespace YazilimMuhendisligiProje_ToDoList.Presentation
             // 
             // frmcoin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1200, 554);
             this.Controls.Add(this.btnFetchData);
             this.Controls.Add(this.dataGridView1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmcoin";
             this.Text = "Crypto Price Viewer";
             this.Load += new System.EventHandler(this.frmcoin_Load_1);
--- YazilimMuhendisligiProje-ToDoList/Program.cs
+++ YazilimMuhendisligiProje-ToDoList/Program.cs
@@ -18,7 +18,7 @@ namespace YazilimMuhendisligiProje_ToDoList
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            Application.Run(new frmLogin());
+            Application.Run(new frmcoin());
 
 
         }
